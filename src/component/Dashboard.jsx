8926caf1ce825a040cb9d7c33da7dbf000d0b956
priyabrata1122 <priyabratass64@gmail.com>
2025-07-17 14:29:08 +0530

    Updation of User Dashboard

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,26 +1,62 @@
-import React, { useContext } from 'react'
-import {FirebaseContext} from '../context/firebase.jsx';
+import React, { useContext, useEffect, useState } from 'react'
+import { FirebaseContext } from '../context/firebase.jsx'
+import { onAuthStateChanged } from 'firebase/auth'
+import user_img from '../assets/user_img.jpg'
 
 function Dashboard() {
 
-	const context=useContext(FirebaseContext);
-	const auth =context.auth;
-	console.log(auth);
+    const context = useContext(FirebaseContext);
+    const auth = context.auth;
+    const [user, setUser] = useState(null);
+    const [img,setImg] = useState(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                setUser(user);
+                setImg(user?.photoURL);
+                console.log(user);
+            }
+            else {
+                setUser(null);
+                setImg(null);
+            }
+        })
+    }, []);
 
     return (
-        <div className='w-[97%] h-auto min-h-[40rem] mt-22 relative left-5'>
+        <div className='w-[97%] h-auto mt-22 relative left-5 flex flex-col items-center'>
 
-			<div className='border-1 border-black rounded-2xl w-full h-auto min-h-[10rem] mt-22 relative '>
-
-			</div>
-
-            <div>
-                <h1></h1>
-                <h2></h2>
+            <div className='rounded-2xl w-[80%] h-auto min-h-[12rem] mt-5 relative flex flex-col items-center justify-center gap-5 p-4 md:flex-row md:justify-evenly md:items-center bg-gray-200 shadow-xl transition-transform duration-200 hover:scale-102'>
+                <img src={img || user_img } alt="Profile Image" className=' h-[5rem] rounded-full md:h-[7rem] lg:h-[10rem]'/>
+                <div className='min-w-[50%] min-h-[10rem] flex flex-col justify-center items-start gap-3'>
+                    <h1 className='font-bold text-xl md:text-2xl lg:text-3xl'>Name : {user?.displayName}</h1>
+                    <h1 className='text-[1rem] lg:text-2xl'>Email : {user?.email}</h1>
+                    <h1 className='text-[1rem] lg:text-2xl'>Hostel : Dhirubhai Ambani Hall of Residence</h1>
+                    <h1 className='text-[1rem] lg:text-2xl'>Category : Student</h1>
+                </div>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
+
+            <h1 className='relative mt-10 font-bold text-4xl'>Services</h1>
+
+            <div className='mt-10  h-auto w-[95%] grid sm:grid-cols-1 md:grid-cols-2 justify-items-center items-center'>
+                <div className='h-[17rem] w-[80%] max-w-[21rem] md:w-[21rem] lg:w-[28rem] mb-10 rounded-2xl hover:shadow-2xl bg-red-200 flex flex-col justify-center items-center text-center transition-transform duration-200 hover:scale-102'>
+                    <h1 className='font-bold text-2xl mb-5'>Borrow Cycle 🚲</h1>
+                    <p>Easily borrow a cycle by <br /> selecting from the available <br /> list and checking it out in seconds.</p>
+                </div>
+                <div className='h-[17rem] w-[80%] max-w-[21rem] md:w-[21rem] lg:w-[28rem] mb-10 rounded-2xl hover:shadow-2xl bg-blue-200 flex flex-col justify-center items-center text-center transition-transform duration-200 hover:scale-102'>
+                    <h1 className='font-bold text-2xl mb-5'>Lend Cycle 🔁</h1>
+                    <p>List your cycle for others to <br /> use and contribute to a smarter, <br /> shared campus commute.</p>
+                </div>
+                <div className='h-[17rem] w-[80%] max-w-[21rem] md:w-[21rem] lg:w-[28rem] mb-10 rounded-2xl hover:shadow-2xl bg-green-100 flex flex-col justify-center items-center text-center transition-transform duration-200 hover:scale-102'>
+                    <h1 className='font-bold text-2xl mb-5'>Repair Cycle 🛠️</h1>
+                    <p>Report issues and send damaged <br /> cycles for repair to keep the <br /> system running smoothly.</p>
+                </div>
+                <div className='h-[17rem] w-[80%] max-w-[21rem] md:w-[21rem] lg:w-[28rem] mb-10 rounded-2xl hover:shadow-2xl bg-yellow-100 flex flex-col justify-center items-center text-center transition-transform duration-200 hover:scale-102'>
+                    <h1 className='font-bold text-2xl mb-5'>View All cycle 📋</h1>
+                    <p>Browse all registered cycles <br /> with live availability and <br /> maintenance status at a glance.</p>
+                </div>
+            </div>
         </div>
     )
 }
